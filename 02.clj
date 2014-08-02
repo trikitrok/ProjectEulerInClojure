30d083b2685ed588f432130eaddcf94cc7939cee
trikitrok <trikitrok@gmail.com>
2014-08-02 22:45:04 +0200

    All intermediate results

--- 02.clj
+++ 02.clj
@@ -1,11 +1,9 @@
-(def fibo (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+(def fibo (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))) ; #'fibo/fibo
 
-(take 12 fibo)
+(take 12 fibo) ; (0 1 2 3 5 8 13 21 34 55 89)
 
-;1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+(take-while (partial > 100) fibo) ; (0 1 2 3 5 8 13 21 34 55 89)
 
-(take-while (partial > 100) fibo)
-
-(filter even? (take-while (partial > 100) fibo))
+(filter even? (take-while (partial > 100) fibo)) ; (0 2 8 34)
 
 (reduce + (filter even? (take-while (partial > 4000000) fibo))) ; 4613732
