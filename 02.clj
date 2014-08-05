87b14b74a310c3a5626b3c0d3703edf4b34eb1dc
trikitrok <trikitrok@gmail.com>
2014-08-05 21:11:44 +0200

    Added some helpers to make it more readable

--- 02.clj
+++ 02.clj
@@ -1,9 +1,17 @@
-(def fibo (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))) ; #'fibo/fibo
+(def fibonaccis
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
 
-(take 12 fibo) ; (0 1 2 3 5 8 13 21 34 55 89)
+(take 12 fibonaccis) ; (0 1 2 3 5 8 13 21 34 55 89)
 
-(take-while (partial > 100) fibo) ; (0 1 2 3 5 8 13 21 34 55 89)
+(take-while (partial > 100) fibonaccis) ; (0 1 2 3 5 8 13 21 34 55 89)
 
-(filter even? (take-while (partial > 100) fibo)) ; (0 2 8 34)
+(filter even? (take-while (partial > 100) fibonaccis)) ; (0 2 8 34)
 
-(reduce + (filter even? (take-while (partial > 4000000) fibo))) ; 4613732
+(reduce + (filter even? (take-while (partial > 4000000) fibonaccis))) ; 4613732
+
+(def evens-in (partial filter even?))
+
+(defn fibonaccis-below [num]
+  (take-while (partial > num) fibonaccis))
+
+(reduce + 0 (evens-in (fibonaccis-below 4000000))) ; 4613732
